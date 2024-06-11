4f96fe313792730694ccd630dacb4bf6a763528c
seifeldin1 <seif1442004@gmail.com>
2024-06-11 14:15:37 +0300

    updated verification in app

--- src/userDetails.tsx
+++ src/userDetails.tsx
@@ -411,7 +411,7 @@ const UserDetailPage:React.FC=()=>{
                         variant="contained"
                         color="primary"
                         onClick={submitHandler}
-                        disabled={!proceed}
+                        disabled={!userInfo.age||!userInfo.city||!userInfo.country||!userInfo.email||!userInfo.firstName||!userInfo.lastName||!userInfo.nationalID||!userInfo.phoneNumber}
                         className="transition ease-in-out duration-150 transform hover:scale-105"
                         >
                           <Link to={proceed?"/home/userInfo/payment":"#"} > Proceed to Payment </Link>  
